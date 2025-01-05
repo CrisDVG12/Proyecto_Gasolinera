681be2d5c652259083535e23e9bac6ecb81d3830
CrisDVG12 <cristianvidal.2003@gmail.com>
2025-01-05 01:11:23 -0400

    Add files via upload

---
+++ gasstation/src/test/java/com/wladimirhernandez/gasstation/GasstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wladimirhernandez.gasstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
