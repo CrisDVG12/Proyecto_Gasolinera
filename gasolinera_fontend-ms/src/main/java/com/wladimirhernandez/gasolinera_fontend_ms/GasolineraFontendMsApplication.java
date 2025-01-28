9ba564a0ef34626035b971e7ae5108a4b6a39366
CrisDVG12 <cristianvidal.2003@gmail.com>
2025-01-28 18:57:13 -0400

    Add files via upload

---
+++ gasolinera_fontend-ms/src/main/java/com/wladimirhernandez/gasolinera_fontend_ms/GasolineraFontendMsApplication.java
@@ -0,0 +1,13 @@
+package com.wladimirhernandez.gasolinera_fontend_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasolineraFontendMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasolineraFontendMsApplication.class, args);
+	}
+
+}
---
+++ gasolinera_fontend-ms/src/test/java/com/wladimirhernandez/gasolinera_fontend_ms/GasolineraFontendMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wladimirhernandez.gasolinera_fontend_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasolineraFontendMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
